63b18f2ce371a4a9fcc4d7503c1644450b203da8
imrazave <imrazave@gmail.com>
2021-04-18 08:36:48 -0500

    added the Aircraft.cs file

---
+++ Avia8r.Data/Aircraft.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avia8r.Data
+{
+    public class Aircraft
+    {
+        [Key]
+        public Guid AcTail { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+        [Required]
+        public string Airline { get; set; }
+    }
+}
