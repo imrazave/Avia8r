adb5c51e12ebcea56ff144104bc98f812252c1b4
imrazave <imrazave@gmail.com>
2021-04-23 17:00:07 -0500

    NavBar Updates

--- Avia8r/Controllers/AircraftController.cs
+++ Avia8r/Controllers/AircraftController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avia8r.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,22 @@ using System.Web.Mvc;
 
 namespace Avia8r.Controllers
 {
+    [Authorize]
     public class AircraftController : Controller
     {
         // GET: Aircraft
         public ActionResult Index()
         {
+            var model = new AircraftListItem[0];
+            return View(model);
+        }
+        //GET
+        public ActionResult Create(AircraftCreate Model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View();
         }
     }
---
+++ Avia8r/Models/AircraftCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Avia8r.Models
+{
+    public class AircraftCreate
+    {
+        [Required]
+        public string AcTail { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+        [Required]
+        public string Airline { get; set; }
+    }
+}
